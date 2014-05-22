54baea471b93be5c8ad5acc7839549811ed25ae0
Jan Kristanto <jan@ontelstudio.com>
2014-05-22 09:36:01 +0700

    fix migration users table

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'larashop',
 			'username'  => 'root',
-			'password'  => 'password',
+			'password'  => 'ontel789',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
